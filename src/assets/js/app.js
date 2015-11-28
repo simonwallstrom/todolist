4c28cd52721d12c7f743fa4123aa2ca3c601bc8c
Simon Wallström <simon.wallstrom@gmail.com>
2015-11-28 15:54:09 +0100

    create edit and delete works

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,9 +2,37 @@
 // Todo app
 //
 
-new Vue ({
-    el: 'body',
+new Vue({
+    el: '#tasks',
+
     data: {
-        message: 'Hello Vue.js!'
+        tasks: [
+            { body: 'Go to the store', completed: false }
+        ],
+        newTask: ''
+    },
+
+    methods: {
+        addTask: function(e) {
+            e.preventDefault();
+
+            this.tasks.push({
+                body: this.newTask,
+                completed: false
+            });
+
+            this.newTask = '';
+        },
+
+        editTask: function(task) {
+            this.removeTask(task);
+            this.newTask = task.body;
+
+            this.$els.hej.focus();
+        },
+
+        removeTask: function(task) {
+            this.tasks.$remove(task);
+        }
     }
 });
